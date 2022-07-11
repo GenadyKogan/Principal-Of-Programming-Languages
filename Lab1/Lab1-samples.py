b1861be53675581c08b11e508548f541cd2b122d
ghgkogan <rgkogan@gmail.com>
2022-07-11 21:16:23 +0300

    change: Lab1-samples.py

--- Lab1/Lab1-samples.py
+++ Lab1/Lab1-samples.py
@@ -106,3 +106,182 @@ while i <= n:
     print(i,end='|')
     i+=1
 """
+
+# =================================================================
+
+x=1
+print('foramt is {0}, id = {1}, and {other}.'.format(type(x), id(x), other ='lab1'))
+
+y=1
+print('x id = {0}, y id = {1}.'.format(id(x), id(y)))
+
+print('x id = {0}, y id = {1}.'.format(id(x), id(y)))
+
+
+
+
+# ==================== Code to swap 'x' and 'y' ====================
+# ==================== 1 ====================
+
+x = 10
+y = 5
+
+
+# x now becomes 15
+x = x + y
+
+# y becomes 10
+y = x - y
+# x becomes 5
+x = x - y
+print("After Swapping: x =", x, " y =", y)
+
+# ==================== 2 ====================
+
+x = 10
+y = 5
+ 
+# code to swap
+# 'x' and 'y'
+ 
+# x now becomes 50
+x = x * y
+ 
+# y becomes 10
+y = x // y
+ 
+# x becomes 5
+x = x // y
+ 
+print("After Swapping: x =",x, " y =", y)
+
+# ==================== 3 ====================
+x = 10
+y = 5
+ 
+# Code to swap 'x' and 'y'
+x = x ^ y; # x now becomes 15 (1111)
+y = x ^ y; # y becomes 10 (1010)
+x = x ^ y; # x becomes 5 (0101)
+ 
+print ("After Swapping: x = ", x, " y =", y)
+
+# ==================== 4 ====================
+
+def swap(xp, yp):
+ 
+    xp[0] = xp[0] ^ yp[0]
+    yp[0] = xp[0] ^ yp[0]
+    xp[0] = xp[0] ^ yp[0]
+ 
+ 
+# Driver code
+x = [10]
+swap(x, x)
+print("After swap(&x, &x): x = ", x[0])
+
+# ==================== 5 ====================
+
+def swap(xp, yp):
+ 
+    # Check if the two addresses are same
+    if (xp[0] == yp[0]):
+        return
+    xp[0] = xp[0] + yp[0]
+    yp[0] = xp[0] - yp[0]
+    xp[0] = xp[0] - yp[0]
+ 
+ 
+# Driver Code
+x = [10]
+swap(x, x)
+print("After swap(&x, &x): x = ", x[0])
+
+
+# ==================== 6 ====================
+ 
+def swap(a, b):
+ 
+    # Same as a = a + b
+    a = (a & b) + (a | b)
+ 
+    # Same as b = a - b
+    b = a + (~b) + 1
+ 
+    # Same as a = a - b
+    a = a + (~b) + 1
+ 
+    print("After Swapping: a = ", a, ", b = ", b)
+ 
+ 
+# Driver code
+a = 5
+b = 10
+ 
+# Function call
+swap(a, b)
+
+# ==================== 7 ====================
+
+def swap(x, y):
+  x , y = y, x
+  print("After Swapping: x = ", x, ", y = ", y)
+   
+# Driver code
+x = 10
+y = 5
+  
+# Function call
+swap(x, y)
+
+# =============================================================
+
+a=b=c=5
+print(a,b,c)
+
+print(7 % 3, 7/3, 7//3, 2**3)
+
+import math
+print(math.pi)
+from math import pi
+print(pi)
+
+print(int("10"))
+
+# format ========================================================
+
+a,b,c = 2, 3.14159265359, 'abcd!@#$'
+
+print('a={0}, b={1}, c={2} next'.format(a,b,c),'b={0:.2f} end'.format(b))
+
+print('{0:>5d},{1:<10.4f},{2}'.format(a,b,c))
+print('{0:^5d},{1:>10.4f},{2}'.format(a,b,c))
+
+#x=int(input('Enter int: '))
+
+# range ==========================================================
+print(tuple(range(4)))
+print(range(4))
+print(tuple(range(3,8)))
+print(tuple(range(8,3,-2)))
+
+# loop =====++=====================================================
+for x in range(1,8):
+	print(x)
+for x in range(1,8):
+	print(x,end=' ')
+for x in range(1,8):
+	print(x,end='$')
+for _ in range(1,8):
+	print('$')
+
+i=1
+n=10
+while i <= n:
+    print(i,end='|')
+    i+=1
+
+print("\n ")
+ACCEPTED_EXTENSIONS     = ('jpg', 'jpeg', 'tif', 'tiff', 'png') 
+
+print(ACCEPTED_EXTENSIONS[0])
